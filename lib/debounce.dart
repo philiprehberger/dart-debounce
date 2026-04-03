9effe2fddb5e7eddb831d3bc26bfe4dcb2924353
Philip Rehberger <me@philiprehberger.com>
2026-04-03 15:50:02 -0600

    Add primary barrel file matching package name

--- lib/debounce.dart
+++ lib/debounce.dart
@@ -1,6 +1,1 @@
-/// Debounce and throttle utilities with Stream transformers and cancellation.
-library;
-
-export 'src/debouncer.dart';
-export 'src/throttler.dart';
-export 'src/stream_transformers.dart';
+export "philiprehberger_debounce.dart";
